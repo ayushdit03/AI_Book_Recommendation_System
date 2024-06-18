945d243a7a7231e5ffdfae7b8af7787cba7d7c80
ayushdit03 <148032098+ayushdit03@users.noreply.github.com>
2024-06-18 16:07:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app = Flask(__name__, static_url_path='/static')
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://pj29102005:bTQfPPqugcyv9mv8@cluster0.9nt5ygc.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster0")
 client = MongoClient(
     MONGO_URI,
-    tls=True,
-    tlsCAFile=certifi.where()
 )
 db = client['library']
 feedback_collection = db['feedback']
